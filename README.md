756dcd5a1a520af2aadc83ff865fcd2e4556c05b
Chris Hitchcott <hitchcott@gmail.com>
2017-04-14 14:52:46 +0800

    [maint] strip down functionality, drop support for non-testrpc

--- README.md
+++ README.md
@@ -1,79 +1,24 @@
 # Tempo
 
-### A tool to simplify time management in EVM Tests
+### A simple tool for time management in truffle testrpc tests
 
-The problem: there are different APIs for traversing time in tests that run against `testrpc` as well as a 'regular' EVM.
-
-Tempo provides a simple interface for 'fast-forwarding' time (mining blocks) that will transparently fallback to regular mining when TestRPC is not available.
-
-An example of a non-TestRPC chain would be a local `geth` instance in `--dev` mode. You could start *that* up with something like this:
-
-```
-geth --dev --rpc --rpcapi="eth,miner,web3" --rpccorsdomain "*" --maxpeers 0 --nodiscover
-```
-
-## Install
-
-```
-npm i --save-dev @digix/tempo
-```
-
-## Usage
 
 ```javascript
-// pass web3 instance to tempo, it will auto-detect whether you're using TestRPC or not
-new Tempo(web3).then((tempo) => {
-    // now you can use `tempo`
-});
+const { wait, waitUntilBlock } = require('@digix/tempo');
+// wait will wait for `n` blocks; (seconds, blocks), default (20, 1)
+// waitUntilBlock will wait until `n` specific block; (seconds, blockNumber)
 
-// API for both TestRPC and regular RPC
-tempo.waitForBlocks(n).then(() => { // mine for x number of blocks from now
-  doSomeCoolStuff();
-});
-const myPromise = tempo.waitUntilBlock(n); // mine until we hit this block number
-
-tempo.currentBlock; // returns latest block, inaccurate if blocks not controlled only by tempo
-// TODO make the above always accurate by adding a `filter.watch`
-
-// API for TestRPC only, which provides a second param
-// `seconds`; set the amount of time in the future (from now) to make the next block
-tempo.waitForBlocks(n, seconds);
-tempo.waitUntilBlock(n, seconds);
-
-// TODO implement snapshots
-tempo.snapshot('snapshotId');
-tempo.restore('snapshotId');
-
-```
-
-## Example
-
-```javascript
-describe('feeDays', function () {
-  // set the 'now' using tempo...
-  it('calculates the correct number of fee days', function () {
-    const numberOfDays = randomInt(12, 40);
-    const numberOfSeconds = numberOfDays * 60 * 60 * 24;
-    let beforeTime;
-    tempo.waitForBlocks(1) // reset the time
-    .then(() => {
-      beforeTime = tempo.currentTimestamp;
-      // check the current time is fine...
-      return digixMath.feeDays.call(beforeTime);
-    }).then((res) => {
-      assert.equal(0, res.toNumber());
-      // zoom a few days into the future
-      return tempo.waitForBlocks(1, numberOfSeconds);
-    })
-    .then(() => digixMath.feeDays.call(beforeTime))
-    .then((res) => assert.equal(numberOfDays, res.toNumber()));
+contract('MockLibraryUser', function () {
+  it('get_last_payment_date returns correct time when its not set', async function () {
+    const mockLibraryUser = await MockLibraryUser.deployed();
+    await mockLibraryUser.create_user(testUser);
+    const lastPayment1 = await mockLibraryUser.get_last_payment_date.call(testUser);
+    const timeDiff1 = new Date().getTime() - (lastPayment1.toNumber() * 1000);
+    assert.ok(timeDiff1 < 1000, 'unset date isnt `now`');
+    await wait();
   });
 });
 ```
-
-## Tests
-
-`npm run test`
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,123 +1,38 @@
-export default class Tempo {
+/* globals web3 */
 
-  constructor(web3) {
-    this.web3 = web3;
-    this.currentBlock = 0;
-    return this._determineApiSupport()
-    .then(this._getCurrentBlock())
-    .then(() => this);
-  }
+function sendRpc(method, params) {
+  return new Promise((resolve) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method,
+      params: params || [],
+      id: new Date().getTime(),
+    }, (err, res) => { resolve(res); });
+  });
+}
 
-  _determineApiSupport() {
-    return new Promise((resolve) => {
-      this.web3.version.getNode((err, node) => {
-        this.testRpcApi = node.includes('TestRPC');
-        resolve();
-      });
-    });
-  }
-
-  _getCurrentBlock() {
-    return new Promise((resolve) => {
-      this.web3.eth.getBlock('latest', (e, block) => {
-        this.currentBlock = block.number;
-        this.currentTimestamp = block.timestamp;
-        resolve();
-      });
-    });
-  }
-
-  // TestRPC Specific
-  _sendRpc(method, params) {
-    return new Promise((resolve) => {
-      this.web3.currentProvider.sendAsync({
-        jsonrpc: '2.0',
-        method,
-        params: params || [],
-        id: new Date().getTime(),
-      }, (err, res) => { resolve(res); });
-    });
-  }
-
-  _testRpcWaitForBlockNumber(targetBlock, secondsToJump) {
-    const logBlock = (n) => {
-      const secondsText = !secondsToJump ? '' : `, fast forward ${secondsToJump} seconds`;
-      // process.stdout.write(`🚀  warped to block ${n}${secondsText}\n`);
-    };
-    const realTargetBlock = secondsToJump ? targetBlock - 1 : targetBlock;
-    return new Promise((resolve) => {
-      const asyncIterator = () => {
-        // minus one block if we have seoncds to jump as we mine again once afterwards
-        if (this.currentBlock >= realTargetBlock) {
-          if (!secondsToJump) {
-            logBlock(this.currentBlock);
-            return resolve();
-          }
-          logBlock(this.currentBlock + 1);
-          return this._sendRpc('evm_increaseTime', [secondsToJump])
-          .then(() => this._sendRpc('evm_mine'))
-          .then(() => this._getCurrentBlock())
-          .then(() => resolve());
+export function waitUntilBlock(seconds, targetBlock) {
+  return new Promise((resolve) => {
+    const asyncIterator = () => {
+      return web3.eth.getBlock('latest', (e, { number }) => {
+        if (number >= targetBlock - 1) {
+          return sendRpc('evm_increaseTime', [seconds])
+          .then(() => sendRpc('evm_mine')).then(resolve);
         }
-        return this._sendRpc('evm_mine')
-        .then(() => this._getCurrentBlock())
-        .then(asyncIterator);
-      };
-      asyncIterator();
-    });
-  }
-
-  _gethWaitForBlockNumber(targetBlock) {
-    return new Promise((resolve) => {
-      let resolved = false;
-      // start mining until we reach the target block...
-      return this._sendRpc('miner_start', [3]).then(() => {
-        // start checking to see if the last block is our block...
-        const filter = this.web3.eth.filter('latest');
-        filter.watch(() => {
-          this._getCurrentBlock().then(() => {
-            if (this.currentBlock >= targetBlock && !resolved) {
-              // process.stdout.write(`⛏  reached block ${this.currentBlock}\n`);
-              resolved = true;
-              filter.stopWatching();
-              this._sendRpc('miner_stop')
-              .then(() => resolve());
-            }
-          });
-        });
+        return sendRpc('evm_mine').then(asyncIterator);
       });
+    };
+    asyncIterator();
+  });
+}
+
+export function wait(seconds = 20, blocks = 1) {
+  return new Promise((resolve) => {
+    return web3.eth.getBlock('latest', (e, { number }) => {
+      resolve(blocks + number);
     });
-  }
-
-  // Public API
-
-  waitForBlocks(blocksToWait, secondsToJump) {
-    return new Promise((resolve) => {
-      this._getCurrentBlock().then(() => {
-        resolve(this.currentBlock + blocksToWait);
-      });
-    })
-    .then((targetBlock) => {
-      return this.waitUntilBlock(targetBlock, secondsToJump);
-    });
-  }
-
-  waitUntilBlock(targetBlock, secondsToJump) {
-    if (this.testRpcApi) {
-      return this._testRpcWaitForBlockNumber(targetBlock, secondsToJump);
-    }
-    return this._gethWaitForBlockNumber(targetBlock);
-  }
-
-  // TODO implement snapshots
-
-  snapshot() {
-
-  }
-  restore() {
-
-  }
-
-  // TODO events listener for polling for block info?
-
+  })
+  .then((targetBlock) => {
+    return waitUntilBlock(seconds, targetBlock);
+  });
 }
