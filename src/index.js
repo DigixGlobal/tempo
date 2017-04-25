539e5d8fd5dbccee684b612f196c848a3133f06f
Chris Hitchcott <hitchcott@gmail.com>
2017-04-25 14:53:32 +0700

    [feature] allow passing web3

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-/* globals web3 */
-
-function sendRpc(method, params) {
-  return new Promise((resolve) => {
-    web3.currentProvider.sendAsync({
-      jsonrpc: '2.0',
-      method,
-      params: params || [],
-      id: new Date().getTime(),
-    }, (err, res) => { resolve(res); });
-  });
-}
-
-export function waitUntilBlock(seconds, targetBlock) {
-  return new Promise((resolve) => {
-    const asyncIterator = () => {
-      return web3.eth.getBlock('latest', (e, { number }) => {
-        if (number >= targetBlock - 1) {
-          return sendRpc('evm_increaseTime', [seconds])
-          .then(() => sendRpc('evm_mine')).then(resolve);
-        }
-        return sendRpc('evm_mine').then(asyncIterator);
-      });
-    };
-    asyncIterator();
-  });
-}
-
-export function wait(seconds = 20, blocks = 1) {
-  return new Promise((resolve) => {
-    return web3.eth.getBlock('latest', (e, { number }) => {
-      resolve(blocks + number);
+module.exports = (web3) => {
+  function sendRpc(method, params) {
+    return new Promise((resolve) => {
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method,
+        params: params || [],
+        id: new Date().getTime(),
+      }, (err, res) => { resolve(res); });
     });
-  })
-  .then((targetBlock) => {
-    return waitUntilBlock(seconds, targetBlock);
-  });
-}
+  }
+  function waitUntilBlock(seconds, targetBlock) {
+    return new Promise((resolve) => {
+      const asyncIterator = () => {
+        return web3.eth.getBlock('latest', (e, { number }) => {
+          if (number >= targetBlock - 1) {
+            return sendRpc('evm_increaseTime', [seconds])
+            .then(() => sendRpc('evm_mine')).then(resolve);
+          }
+          return sendRpc('evm_mine').then(asyncIterator);
+        });
+      };
+      asyncIterator();
+    });
+  }
+  function wait(seconds = 20, blocks = 1) {
+    return new Promise((resolve) => {
+      return web3.eth.getBlock('latest', (e, { number }) => {
+        resolve(blocks + number);
+      });
+    })
+    .then((targetBlock) => {
+      return waitUntilBlock(seconds, targetBlock);
+    });
+  }
+  return { wait, waitUntilBlock };
+};
